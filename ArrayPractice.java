dae31b04a63ca8896f782edbb07e92720d475c00
rafdevv <rflplays0@gmail.com>
2026-06-07 21:40:10 +0800

    commit 1234567890abcdef1234567890abcdef12345678

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -1,23 +1,23 @@
 public class ArrayPractice {
     public static void main(String[] args) {
 
-        // === Task 1: Array of 5 names and print each ===
+        
         String[] names = {"Sunshine", "Cruz", "Kristina", "Diana", "Ronald"};
         System.out.println("Names in the array:");
         for (String name : names) {
             System.out.println(name);
         }
 
-        // === Task 2: Modify second element of int array and print updated array ===
+        
         int[] numbers = {10, 20, 30, 40, 50};
-        numbers[1] = 99;  // Modify second element (index 1)
+        numbers[1] = 99;  
 
         System.out.println("\nUpdated integer array:");
         for (int num : numbers) {
             System.out.print(num + " ");
         }
 
-        // === Task 3: Find max value in an array ===
+        
         int[] data = {12, 45, 7, 89, 34, 67};
         int max = data[0];
 
--- NumberLoop.java
+++ NumberLoop.java
@@ -1,11 +1,11 @@
 public class NumberLoop {
     public static void main(String[] args) {
-        // For loop from 1 to 10
+        /
         for (int i = 1; i <= 10; i++) {
-            // Print the current number
+            
             System.out.println(i);
 
-            // Stop the loop when number 7 is reached
+            
             if (i == 7) {
                 break;
             }
